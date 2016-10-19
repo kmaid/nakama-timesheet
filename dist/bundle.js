730cb74574b3f292d34ed9ddf49d619d8d79f13d
Thomas Waterfall <thomas.waterfall@gmail.com>
2016-10-19 22:15:06 +0100

    Updated positioning

--- dist/bundle.js
+++ dist/bundle.js
@@ -46,10 +46,55 @@
 
 	__webpack_require__(1);
 	var doc = new jsPDF("l");
-	doc.setFontSize(40);
+	doc.setFontSize(10);
 	doc.addImage(__webpack_require__(5), 'jpg', 0, 0, doc.internal.pageSize.width, doc.internal.pageSize.height);
-	doc.text(35, 25, "Octonyan loves jsPDF");
-	doc.save("test.pdf");
+	doc.text(28, 50.5, "Thomas Waterfall");
+	doc.text(33, 59, "Never Bland");
+	doc.text(36, 66.5, "Annika Hart");
+	doc.text(41, 74.5, "0123456789");
+	doc.text(44, 82, "01/02/66");
+	doc.text(48, 90, "12345678");
+	
+	doc.text(76.5, 105, "X");
+	doc.text(93.2, 105, "X");
+	
+	doc.setFontSize(8);
+	doc.text(132, 58.4, "10 20 30");
+	doc.text(132, 66.5, "10 20 30");
+	doc.text(132, 74.2, "10 20 30");
+	doc.text(132, 81.8, "10 20 30");
+	doc.text(132, 89.4, "10 20 30");
+	doc.text(132, 97.5, "10 20 30");
+	doc.text(132, 105.4, "10 20 30");
+	
+	doc.text(157.5, 58.4, "9 30");
+	doc.text(157.5, 66.5, "9 30");
+	doc.text(157.5, 74.2, "9 30");
+	doc.text(157.5, 81.8, "9 30");
+	doc.text(157.5, 89.4, "9 30");
+	doc.text(157.5, 97.5, "9 30");
+	doc.text(157.5, 105.4, "9 30");
+	
+	doc.text(179, 58.4, "6 30");
+	doc.text(179, 66.5, "6 30");
+	doc.text(179, 74.2, "6 30");
+	doc.text(179, 81.8, "6 30");
+	doc.text(179, 89.4, "6 30");
+	doc.text(179, 97.5, "6 30");
+	doc.text(179, 105.4, "6 30");
+	
+	doc.text(201.2, 58.4, "1 00");
+	doc.text(201.2, 66.5, "1 00");
+	doc.text(201.2, 74.2, "1 00");
+	doc.text(201.2, 81.8, "1 00");
+	doc.text(201.2, 89.4, "1 00");
+	doc.text(201.2, 97.5, "1 00");
+	doc.text(201.2, 105.4, "1 00");
+	
+	
+	doc.output('datauri');
+	
+	// doc.save("test.pdf");
 
 /***/ },
 /* 1 */
